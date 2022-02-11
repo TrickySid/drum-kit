7517935d57c683cc7cccdd20a7ee082728f16286
Siddhesh Bakre <85791341+TrickySid@users.noreply.github.com>
2022-02-11 19:38:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        buttonPlayed = this.innerHTML;
+        playSound(buttonPlayed);
+        buttonAnimation(buttonPlayed);
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+        playSound(event.key);
+        buttonAnimation(event.key);
+});
+
+function playSound(buttonPlayed) {
+    switch (buttonPlayed) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            console.log(buttonPlayed);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    // ("." + currentKey) to check the class of drum buttons
+    // ".pressed" class (declared in CSS) is applied to animate the button by adding it here on getting triggered
+    var activeButton = document.querySelector("." + currentKey).classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton = document.querySelector("." + currentKey).classList.remove("pressed");
+    }, 100);
+}
